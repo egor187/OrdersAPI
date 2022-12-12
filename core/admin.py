6f1c365ec885ff4a90d526ecbde21dd317546610
Yehor Osipovsky <e.osipovsky@volsor.com>
2022-12-12 18:01:10 +0300

    make 'completed_at' field of Campaign read_only in admin

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Client, Message, Campaign
 
+
+class CampaignAdmin(admin.ModelAdmin):
+    readonly_fields = ('completed_at',)
+
+
 admin.site.register(Client)
 admin.site.register(Message)
-admin.site.register(Campaign)
+admin.site.register(Campaign, CampaignAdmin)
