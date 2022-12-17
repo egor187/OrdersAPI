7c6974162a5d226fc74752fb07fd2807d8d86f35
Yehor Osipovsky <e.osipovsky@volsor.com>
2022-12-17 12:40:41 +0300

    prefetch related objs in celery_task

--- core/tasks.py
+++ core/tasks.py
@@ -13,10 +13,11 @@ from .connectors import BaseAPIConnector
     max_retries=10,
 )
 def run_campaign(campaign_settings, campaign_finished_at):
-    clients = Client.objects.filter(**campaign_settings)
-    while timezone.now() < campaign_finished_at:
+    clients = Client.objects.filter(**campaign_settings).prefetch_related('messages')
+    while timezone.now() < campaign_finished_at and clients:
         for client in clients:
             client_messages = client.messages.all()
             for message in client_messages:
                 connector = BaseAPIConnector(client=client, message=message)
                 connector.send_request()
+            clients = clients.exclude(pk=client.pk)
